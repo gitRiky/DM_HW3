54c72489d838ff19ce83a04af849951554986d6d
Riccardo <distefanor3@gmail.com>
2017-01-02 16:16:32 +0100

    ATTEMPT TO IMPLEMENT THE ALGORITHM: 1) not sure about the counting of least-significant 1; 2) not sure about the hash function

--- frajolet-martin.py
+++ frajolet-martin.py
@@ -1,8 +1,8 @@
 from math import log
+import codecs
 
-NUM_ELEMENTS = 512
+NUM_ELEMENTS = 32
 LENGTH_OF_BITMAP = int(log(NUM_ELEMENTS, 2))
-BITMAP = "0" * LENGTH_OF_BITMAP
 
 
 def get_binary_representation(number):
@@ -16,7 +16,10 @@ def get_binary_representation(number):
 def get_least_sign_bit(number):
     if number == 0:
         return LENGTH_OF_BITMAP
-    b_number = get_binary_representation(number)
+    if type(number) is str:
+        b_number = number
+    else:
+        b_number = get_binary_representation(number)
     counter = -1
     for index in b_number:
         counter += 1
@@ -24,5 +27,31 @@ def get_least_sign_bit(number):
             return counter
     return 0
 
-print BITMAP
 
+def my_hash(element):
+    return hash(element) % LENGTH_OF_BITMAP
+
+
+def estimate_f0(stream):
+    bitmap = []
+    for i in range(0, LENGTH_OF_BITMAP):
+        bitmap.append('0')
+    for elem in stream:
+        h_elem = my_hash(elem)
+        if h_elem != 0:
+            lsb = get_least_sign_bit(h_elem)
+            print "elem " + elem + " with lsb = ", lsb
+            if bitmap[lsb] == '0':
+                bitmap[lsb] = '1'
+            print bitmap
+    bitmap = "".join(bitmap)
+    estimation = 2 ** (LENGTH_OF_BITMAP - 1 - get_least_sign_bit(bitmap))
+    return estimation
+
+
+def main():
+    with codecs.open("input2.txt", 'r') as file_handle:
+        result = estimate_f0(file_handle)
+    print "The number of estimated different elements is ", result
+
+main()
