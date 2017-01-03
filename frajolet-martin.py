5a2af8058e74a2edce88c0dd2af6357ff1e934c2
Riccardo <distefanor3@gmail.com>
2017-01-03 12:08:13 +0100

    I implemented the following methods: 1) estimate_f0, which estimate f0 using a single hash function (my_hash); 2) bf_duplicates, that count in a brute force way the distinct elements (remove later the duplicates counter); 3) ip_file used for creating a file of only ips and addresses from the access_log_Jul95 dataset.

--- frajolet-martin.py
+++ frajolet-martin.py
@@ -1,57 +1,84 @@
-from math import log
 import codecs
 
-NUM_ELEMENTS = 32
-LENGTH_OF_BITMAP = int(log(NUM_ELEMENTS, 2))
+LENGTH_BITMAP = 32
 
 
 def get_binary_representation(number):
     binary = "{0:b}".format(number)
-    while len(binary) < LENGTH_OF_BITMAP:
-        binary = "0" + binary;
+    while len(binary) < LENGTH_BITMAP:
+        binary = "0" + binary
     return binary
 
 
-# returns the position of the least significant bit (counting from left to right)
+# least significant one position, it is equal to the length of the tail
 def get_least_sign_bit(number):
     if number == 0:
-        return LENGTH_OF_BITMAP
+        return LENGTH_BITMAP
     if type(number) is str:
         b_number = number
     else:
         b_number = get_binary_representation(number)
-    counter = -1
-    for index in b_number:
-        counter += 1
-        if index == "1":
-            return counter
-    return 0
+    # print "Binary representation of hash %d is equal to %s" % (number, b_number)
+    return LENGTH_BITMAP - 1 - b_number.rfind("1")
 
 
 def my_hash(element):
-    return hash(element) % LENGTH_OF_BITMAP
+    return (19*hash(element) +1) % 2**32
 
 
 def estimate_f0(stream):
     bitmap = []
-    for i in range(0, LENGTH_OF_BITMAP):
+    max_tail = 0
+    for i in range(0, LENGTH_BITMAP):
         bitmap.append('0')
     for elem in stream:
+        elem = elem.replace("\n", "")
+        # print "I'm considering element " + elem
         h_elem = my_hash(elem)
         if h_elem != 0:
             lsb = get_least_sign_bit(h_elem)
-            print "elem " + elem + " with lsb = ", lsb
-            if bitmap[lsb] == '0':
-                bitmap[lsb] = '1'
-            print bitmap
-    bitmap = "".join(bitmap)
-    estimation = 2 ** (LENGTH_OF_BITMAP - 1 - get_least_sign_bit(bitmap))
+            # print "Least significant 1 bit = ", lsb
+            # tail = len - 1 - lsb;
+            tail = lsb
+            if tail > max_tail:
+                max_tail = tail
+    print "Max tail = ", max_tail
+    estimation = 2 ** max_tail
+    print "We know that the estimation has to be equal to 2 ^ max_tail"
     return estimation
 
 
 def main():
-    with codecs.open("input2.txt", 'r') as file_handle:
+    with codecs.open("ip.txt", 'r') as file_handle:
+        bf_duplicates(file_handle)
         result = estimate_f0(file_handle)
     print "The number of estimated different elements is ", result
+
+
+def ip_file():
+    counter = 0
+    with codecs.open("access_log_Jul95", 'r') as file_handle:
+        with codecs.open("ip.txt", 'w') as f_handle:
+            for line in file_handle:
+                counter += 1
+                if counter > 8000:
+                    break
+                split = line.split(" - - ")
+                f_handle.write(split[0])
+                f_handle.write("\n")
+
+
+def bf_duplicates(file_handle):
+    dict = {}
+    for line in file_handle:
+        line = line.replace("\n","")
+        if dict.has_key(line):
+            dict[line] = dict[line] + 1
+        else:
+            dict[line] = 1
+    for elem in dict.keys():
+        print "elem " + elem + ", duplicates = ", dict[elem]
+    print "number of different elements = ", len(dict.keys())
+
 
 main()
