1646b1ed8dd4910e1f7f9d76d842a5e399ad0413
Riccardo <distefanor3@gmail.com>
2017-03-07 12:58:57 +0100

    first commit

--- AndroidStudioProjects/Beacon/app/src/main/java/com/example/riccardo/beacon/MainActivity.java
+++ AndroidStudioProjects/Beacon/app/src/main/java/com/example/riccardo/beacon/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.riccardo.handsonexampleestimote;
+package com.example.riccardo.beacon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
